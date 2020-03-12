43627c88953259f31bbc26e21dda42928c7dba1f
Kirill Leogky <kirillmargheriti@gmail.com>
2020-03-12 19:21:37 +0300

    feat: add redux devtools

--- datagrid/src/store/store.js
+++ datagrid/src/store/store.js
@@ -1,4 +1,4 @@
 import {combineReducers, createStore} from 'redux';
 import reducer from '../reducers'
 
-export default createStore(combineReducers(reducer))
+export default createStore(combineReducers(reducer), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
